5285a44e14ede3304a8471096ef2e02e19fd854b
SoulZeroTwo <contactjonathanevoy@gmail.com>
2025-11-23 16:38:44 -0500

    Fini
    Le site est pas mal fini et prêt pour la distribution

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,6 +1,28 @@
 import Icons from './utils/Icons.js';
 import Swiper from 'swiper/bundle';
 
+const swiper = new Swiper('.js-swiper', {
+  speed: 400,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  breakpoints: {
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 1,
+    },
+  },
+});
+
 class Main {
   constructor() {
     this.init();
